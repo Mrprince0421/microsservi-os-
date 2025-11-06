f4107b43464f86d95479b4e78354fa0c592e40ef
Marcos Eduardo <120418072+Mrprince0421@users.noreply.github.com>
2025-11-06 00:13:24 -0300

    kk

--- User/app/DB.py
+++ User/app/DB.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from .settings import Settings
 
 engine = create_engine(Settings().DATABASE_URL)
